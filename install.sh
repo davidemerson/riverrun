525ab33ae16db54777506c1455a4131a19523a83
David Emerson <d@nnix.com>
2025-01-16 22:12:31 -0500

    -p

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ mkdir /usr/bin/riverrun
 cp -R converter/ /usr/bin/riverrun/converter
 cp -R streamer/ /usr/bin/riverrun/streamer
 
-mkdir /var/music/uploads
-mkdir /var/music/queue
+mkdir -p /var/music/uploads
+mkdir -p /var/music/queue
